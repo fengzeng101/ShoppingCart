2d819589d081055926cc0e3e8dda1db48724697e
fengzeng101 <97267228+fengzeng101@users.noreply.github.com>
2022-04-15 11:08:59 +1000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.get("/",(req,res)=>{
-    res.send("Welcome to our online shop API111...")
+    res.send("Welcome to our online shop API...")
 })
 
 app.get("/products",(req,res)=>{
