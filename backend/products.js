e2fe4c722780c9bfe6c00e5b846934fe377d2cdc
fengzeng101 <fengzeng@hotmail.com>
2022-04-15 17:05:34 +1000

    add toastify

--- backend/products.js
+++ backend/products.js
@@ -7,14 +7,14 @@ const products = [
         image:"https://res.cloudinary.com/dmkadw8wn/image/upload/v1649932543/shoppingCart/iphone12pro_byvvmh.jpg"
     },
     {
-        id:1,
+        id:2,
         name:"iPhone 12",
         desc:"5.4-inch display",
         price:699,
         image:"https://res.cloudinary.com/dmkadw8wn/image/upload/v1649932543/shoppingCart/iphone12_rkn6jw.jpg"
     },
     {
-        id:1,
+        id:3,
         name:"Galaxy S",
         desc:"6.5-inch display",
         price:399,
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import productsReducer, { productsFetch } from "./features/productsSlice";
 import { productsApi } from './features/productsApi';
+import cartReducer from "./features/cartSlice"
 
 const store = configureStore({
   reducer:{
     products:productsReducer,
+    cart:cartReducer,
     [productsApi.reducerPath]:productsApi.reducer,
   },
   middleware:(getDefaultMiddleware)=>{
