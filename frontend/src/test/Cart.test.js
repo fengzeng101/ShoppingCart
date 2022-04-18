aac78b84672dc711b0f072c24e615be36ae12b29
fengzeng101 <fengzeng@hotmail.com>
2022-04-18 23:21:22 +1000

    working on the testing case

--- frontend/src/test/Cart.test.js
+++ frontend/src/test/Cart.test.js
@@ -7,6 +7,9 @@ import { Provider } from 'react-redux';
 import store from "../store";
 import '@testing-library/jest-dom';
 import {BrowserRouter} from "react-router-dom";
+import * as actions from "../features/cartSlice";
+import Enzyme from 'enzyme';
+
 
 
 const render = component => rtlRender(
@@ -24,6 +27,35 @@ describe("Cart", () => {
     test('Cart loading properly', ()=>{
     render(<Cart/>)
     expect(screen.getByText('Shopping Basket Checkout')).toBeInTheDocument();
+
+    // it('cart item number', () => {
+
+    //   const mockStore={ 'quantity'};
+    //   const dispatchSpy = jest.spyOn(ReactRedux, 'useDispatch');
+    //   const mockDispatch = jest.fn();
+    //   dispatchSpy.mockReturnValue(mockDispatch);
+  
+  
+    //   const addToCartMock = jest.fn();
+    //   const addToCartSpy = jest.spyOn(actions, 'addToCart');
+    //   addToCartSpy.mockImplementation(addToCartMock);
+  
+    //   // const wrapper = Enzyme.mount(<Provider store={store} >
+    //   //   <Cart />
+    //   // </Provider>);
+    //   // expect(wrapper.find('button')).toHaveLength(0);
+  
+    //   render(<Provider store={mockStore} >
+    //     <Cart />
+    //   </Provider>);
+  
+    //   const backHomeLink = screen.getByText('Back To Home')
+  
+    //   fireEvent.click(backHomeLink);
+  
+    //   expect(screen.getByText('Thank You For Shopping')).toBeInTheDocument();
+
+
 
     });
   
